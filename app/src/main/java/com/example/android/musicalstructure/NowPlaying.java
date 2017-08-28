89161f275a6093df59d56aeaf1ebd8057846554d
Adam <adamparker05@gmail.com>
2017-08-28 08:10:32 -0400

    made revisions

--- app/src/main/java/com/example/android/musicalstructure/NowPlaying.java
+++ app/src/main/java/com/example/android/musicalstructure/NowPlaying.java
@@ -1,10 +1,8 @@
 package com.example.android.musicalstructure;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
